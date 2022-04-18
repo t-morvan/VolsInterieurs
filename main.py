9e8e17e82bc1eb2396c457dece1015a4e623f8ef
titouan <titouan.morvan1@gmail.com>
2022-04-18 20:26:42 +0200

    clean + typing

--- main.py
+++ main.py
@@ -1,53 +1,62 @@
-import yaml 
 import logging
-import pandas as pd
-from tqdm import tqdm
-from utils import *
 
-if __name__=="__main__" :
+import pandas as pd
+import yaml
+from tqdm import tqdm
+
+from utils import getDuration, get_stations, get_coords, get_flights, in_metro
+
+
+def main() -> None:
 
     # API params
-   with open('config.yaml', 'r') as f:
-      params_API =  yaml.safe_load(f)
+    with open("config.yaml", "r", encoding="utf-8") as f:
+        params_API = yaml.safe_load(f)
 
-   # data
-   flights = get_flights()
-   stations = get_stations()
+    # data
+    flights = get_flights()
+    stations = get_stations()
 
-   # setup
-   train_duration = getDuration(**params_API)
-   logging.basicConfig(filename='data/geocode.log', level=logging.INFO)
+    # setup
+    train_duration = getDuration(**params_API)
+    logging.basicConfig(filename="data/geocode.log", level=logging.INFO)
 
-   links =[]  
-   for flight in tqdm(flights.itertuples(), total = flights.shape[0]):
-      
-      # geocode departure/arrival
-      try :
-         lat_d, lon_d = get_coords(flight.departure)
-         lat_a, lon_a = get_coords(flight.arrival)
-      except TypeError:
-         logging.warning(f"Geocode error : {flight.departure}-{flight.arrival}")
-         continue
+    links = []
+    for flight in tqdm(flights.itertuples(), total=flights.shape[0]):
 
-      # keep only metro flight
-      if not in_metro(lat_d, lon_d) or not in_metro(lat_a, lon_a ):
-         continue
+        # geocode departure/arrival
+        try:
+            lat_d, lon_d = get_coords(flight.departure)
+            lat_a, lon_a = get_coords(flight.arrival)
+        except TypeError:
+            logging.warning("Geocode error : %s-%s}", flight.departure, flight.arrival)
+            continue
 
-      # compute train duration
-      departure_station = stations[flight.departure]
-      arrival_station = stations[flight.arrival]
-      duration = train_duration.compute(departure_station, arrival_station)
+        # keep only metro flight
+        if not in_metro(lat_d, lon_d) or not in_metro(lat_a, lon_a):
+            continue
 
-      links.append({
-         'departure' : flight.departure,
-         'arrival' : flight.arrival, 
-         'lat_d' : lat_d,
-         'lon_d' : lon_d,
-         'lat_a' : lat_a,
-         'lon_a' : lon_a,
-         'duration' : duration
-      })
-      
-   links_df = pd.DataFrame(links)
-   links_df = links_df.merge(flights, on=['departure', 'arrival'])
-   links_df.to_csv("data/links.csv")
+        # compute train duration
+        departure_station = stations[flight.departure]
+        arrival_station = stations[flight.arrival]
+        duration = train_duration.compute(departure_station, arrival_station)
+
+        links.append(
+            {
+                "departure": flight.departure,
+                "arrival": flight.arrival,
+                "lat_d": lat_d,
+                "lon_d": lon_d,
+                "lat_a": lat_a,
+                "lon_a": lon_a,
+                "duration": duration,
+            }
+        )
+
+    links_df = pd.DataFrame(links)
+    links_df = links_df.merge(flights, on=["departure", "arrival"])
+    links_df.to_csv("data/links.csv")
+
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,70 +1,83 @@
+"""
+Utils to load data and call the SNCF API
+"""
 
-import requests
-import pandas as pd
-import re
 import logging
+from typing import Dict, Union, Tuple
 
-URL_OSM = 'https://nominatim.openstreetmap.org/search/' 
+import pandas as pd
+import requests
+
+URL_OSM = "https://nominatim.openstreetmap.org/search/"
 URL_SNCF = "https://api.sncf.com/v1/coverage/sncf/journeys"
-FR_BOX = [-7.382813,41.129021,11.403809,51.082822]
+FR_BOX = [-7.382813, 41.129021, 11.403809, 51.082822]
+
 
 class getDuration:
+    """
+    Compute journey duration between two stations given by their adminisrative code
+    """
 
-    def __init__(self, key, date):
+    def __init__(self, key: str, date: str) -> None:
         self.session = requests.Session()
-        self.session.auth = (key,None)
+        self.session.auth = (key, None)
         self.date = date
 
-    def make_params(self, departure, arrival):
-        return { 
-        'datetime' : self.date, 
-        'datetime_represents' : "departure", 
-        'timeframe_duration' : 86400,
-        'from' : "admin:fr:{}".format(departure), 
-        'to' : "admin:fr:{}".format(arrival)
+    def make_params(self, departure: str, arrival: str) -> Dict[str, Union[str, int]]:
+        return {
+            "datetime": self.date,
+            "datetime_represents": "departure",
+            "timeframe_duration": 86400,
+            "from": f"admin:fr:{departure}",
+            "to": f"admin:fr:{arrival}",
         }
 
-    def compute(self, departure, arrival):
+    def compute(self, departure: str, arrival: str) -> float:
         params = self.make_params(departure, arrival)
         res = self.session.get(url=URL_SNCF, params=params)
         if res.status_code == 200:
-            trajets = res.json()['journeys']
-            return min(trajet['duration'] for trajet in trajets)
-        else :
-            logging.warning(f"SNCF API error {res.status_code} : {departure}-{arrival}")
-            return float('nan')
-    
-def get_flights():
+            trajets = res.json()["journeys"]
+            return min(trajet["duration"] for trajet in trajets)
+        else:
+            logging.warning(
+                "SNCF API error %s : %s-%s", res.status_code, departure, arrival
+            )
+            return float("nan")
+
+
+def get_flights() -> pd.DataFrame:
     #  lecture data
     flights = pd.read_csv("data/flights.csv")
-    flights = flights.replace(":",0)
+    flights = flights.replace(":", 0)
     flights = flights[:-3]
     # sélection des vols intérieurs (dont DOM-TOM)
-    flights =  flights[flights.CODE.str.match(r"FR\_\w+\_FR\w+")]
-    # avant 2001 pas de distinction entre Roissy et Orly, juste Airport System, Paris Airport      
+    flights = flights[flights.CODE.str.match(r"FR\_\w+\_FR\w+")]
+    # avant 2001 pas de distinction entre Roissy et Orly, juste Airport System, Paris Airport
     flights["PAIR"] = flights["PAIR"].str.replace("airport SYSTEM - ", "")
     flights["PAIR"] = flights["PAIR"].str.replace("airport", "")
     # extraction arrivee/depart
-    flights[['departure', 'arrival']] = flights['PAIR'].str.split(' - ', 1, expand=True)
-    flights['departure'] = flights['departure'].str.slice(stop=-1)
-    flights['arrival'] = flights['arrival'].str.slice(stop=-1)
+    flights[["departure", "arrival"]] = flights["PAIR"].str.split(" - ", 1, expand=True)
+    flights["departure"] = flights["departure"].str.slice(stop=-1)
+    flights["arrival"] = flights["arrival"].str.slice(stop=-1)
     return flights
 
-def get_stations():
+
+def get_stations() -> Dict[str, str]:
     # crée un dictionnaire aéroport - code administratif de la ville associée
-    stations = pd.read_csv("data/gares.csv", dtype={'code': str}, index_col="aeroport")
+    stations = pd.read_csv("data/gares.csv", dtype={"code": str}, index_col="aeroport")
     return stations["code"].to_dict()
 
-def get_coords(place):
-    params = {'q' : place, 'format' : 'json'}
+
+def get_coords(place: str) -> Union[None, Tuple[float, float]]:
+    params = {"q": place, "format": "json"}
     response = requests.get(URL_OSM, params=params).json()
     if response:
         return response[0]["lat"], response[0]["lon"]
     else:
         return None
 
-def in_metro(lat, lon):
-    lon=float(lon)
-    lat=float(lat)
-    return (lon>FR_BOX[0]) & (lat>FR_BOX[1]) & (lon<FR_BOX[2]) & (lat<FR_BOX[3])
 
+def in_metro(lat: float, lon: float) -> bool:
+    lon = float(lon)
+    lat = float(lat)
+    return (lon > FR_BOX[0]) & (lat > FR_BOX[1]) & (lon < FR_BOX[2]) & (lat < FR_BOX[3])
